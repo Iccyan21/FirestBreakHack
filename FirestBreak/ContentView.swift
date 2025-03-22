37184e4dd20340981b38717a4a7264514b4c2084
nekonlu <oohara.yoji@gmail.com>
2025-03-22 20:58:19 +0900

    プロフィール詳細画面を実装した

--- FirestBreak/ContentView.swift
+++ FirestBreak/ContentView.swift
@@ -556,8 +556,6 @@ struct ImagePicker: UIViewControllerRepresentable {
     }
 }
 // MARK: - Preview Provider
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview {
+    ContentView()
 }
